d3e43ba6149da810c73e2bd8acb1b35ad1b03f6d
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-09 13:22:51 -0700

    Update code.js

--- code.js
+++ code.js
@@ -153,7 +153,7 @@ function measureRuntime(algorithm, distanceMatrix) {
     return endTime - startTime;
 }
 
-let sizes = [5, 10, 15, 20, 25, 30];
+let sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 let heldKarpTimes = [];
 let localSearchTimes = [];
 
